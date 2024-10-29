49e8f0c3e455cc22eb7c3669522520e231f9de8d
Luiz Gabriel Dos Santos Nogueira <luizgabrielsnn@gmail.com>
2024-10-29 16:06:00 -0300

    004

---
+++ resources/views/layouts/main_layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>TEXTO DO LAYOUT PRIMEIRO</p>
+    <hr>
+    
+    @yield('content')
+    
+    <hr>
+    <p>TEXTO DO LAYOUT ULTIMO</p>
+</body>
+</html>
--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
     <h1>Welcome View and Blade {{ $value }} </h1>
+    <h3>Page 2</h3>
 @endsection
